bfa65af4f847c042f4a7f3f69659d28c9fb1839d
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2023-05-23 16:28:06 +0700

    Added binding of shapes to the grid

--- src/components/chessBoard/ChessBoard.tsx
+++ src/components/chessBoard/ChessBoard.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, {useRef, useState} from 'react';
 import Tile from "../tile/Tile";
 
 import "./ChessBoard.css";
@@ -12,39 +12,44 @@ interface Piece {
     y: number;
 }
 
-const pieces: Piece[] = [];
+const initialBoardState: Piece[] = [];
 
 for (let i = 0; i < horizontalAxis.length; ++i) {
-    pieces.push({image: "assets/images/pawn_b.png", x: i, y: 6});
+    initialBoardState.push({image: "assets/images/pawn_b.png", x: i, y: 6});
 }
 
 for (let i = 0; i < horizontalAxis.length; ++i) {
-    pieces.push({image: "assets/images/pawn_w.png", x: i, y: 1});
+    initialBoardState.push({image: "assets/images/pawn_w.png", x: i, y: 1});
 }
 
 for (let i = 0; i < 2; ++i) {
     const type = i === 0 ? "b" : "w";
     const y = i === 0 ? 7 : 0;
 
-    pieces.push({image: `assets/images/rook_${type}.png`, x: 0, y});
-    pieces.push({image: `assets/images/rook_${type}.png`, x: 7, y});
-    pieces.push({image: `assets/images/knight_${type}.png`, x: 1, y});
-    pieces.push({image: `assets/images/knight_${type}.png`, x: 6, y});
-    pieces.push({image: `assets/images/bishop_${type}.png`, x: 2, y});
-    pieces.push({image: `assets/images/bishop_${type}.png`, x: 5, y});
-    pieces.push({image: `assets/images/queen_${type}.png`, x: 3, y});
-    pieces.push({image: `assets/images/king_${type}.png`, x: 4, y});
+    initialBoardState.push({image: `assets/images/rook_${type}.png`, x: 0, y});
+    initialBoardState.push({image: `assets/images/rook_${type}.png`, x: 7, y});
+    initialBoardState.push({image: `assets/images/knight_${type}.png`, x: 1, y});
+    initialBoardState.push({image: `assets/images/knight_${type}.png`, x: 6, y});
+    initialBoardState.push({image: `assets/images/bishop_${type}.png`, x: 2, y});
+    initialBoardState.push({image: `assets/images/bishop_${type}.png`, x: 5, y});
+    initialBoardState.push({image: `assets/images/queen_${type}.png`, x: 3, y});
+    initialBoardState.push({image: `assets/images/king_${type}.png`, x: 4, y});
 }
 
 const ChessBoard = () => {
+    const [activePiece, setActivePiece] = useState<HTMLElement | null>(null);
+    const [gridX, setGridX] = useState(0);
+    const [gridY, setGridY] = useState(0);
+    const [pieces, setPieces] = useState<Piece[]>(initialBoardState);
     const chessBoardRef = useRef<HTMLDivElement>(null);
-
-    let activePiece: HTMLElement | null = null;
 
     const grabPiece = (e: React.MouseEvent) => {
         const element = e.target as HTMLElement;
 
-        if (element.classList.contains("chess-piece")) {
+        if (element.classList.contains("chess-piece") && chessBoardRef.current) {
+            setGridX(Math.floor((e.clientX - chessBoardRef.current.offsetLeft) / 80));
+            setGridY(Math.abs(Math.ceil((e.clientY - chessBoardRef.current.offsetTop - 640) / 80)));
+
             const x = e.clientX - 40;
             const y = e.clientY - 40;
 
@@ -52,7 +57,7 @@ const ChessBoard = () => {
             element.style.left = `${x}px`;
             element.style.top = `${y}px`;
 
-            activePiece = element;
+            setActivePiece(element);
         }
     }
 
@@ -88,8 +93,21 @@ const ChessBoard = () => {
     }
 
     const dropPiece = (e: React.MouseEvent) => {
-        if (activePiece) {
-            activePiece = null;
+        if (activePiece && chessBoardRef.current) {
+            const x = Math.floor((e.clientX - chessBoardRef.current.offsetLeft) / 80);
+            const y = Math.abs(Math.ceil((e.clientY - chessBoardRef.current.offsetTop - 640) / 80));
+
+            setPieces((prev) => {
+                return prev.map((piece) => {
+                    if (piece.x === gridX && piece.y === gridY) {
+                        piece.x = x;
+                        piece.y = y;
+                    }
+                    return piece;
+                });
+            });
+
+            setActivePiece(null);
         }
     }
 
